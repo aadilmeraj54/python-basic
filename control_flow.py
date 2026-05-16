5f69cbe579a64aa5117482c01b33a46a795318d2
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-17 00:30:22 +0530

    Create control_flow.py

---
+++ control_flow.py
@@ -0,0 +1,59 @@
+marks_aadil = 59
+if marks_aadil>60:
+    print("Allowed to take exam")
+    print("i am print from inside if condoition")
+else:
+    print("i am printing from my else part if my condition got failed")    
+print("i dont care bout if and else i will always say heyto you")    
+
+name1 = "hero"
+name2 = "hero"
+if (name1==name2):
+    print ("same name")
+
+else:
+    print("different name")    
+
+a = 89
+b = 10
+if (a/b):    #####if my value is on zero then it is consider as true
+    print("yo")
+    a=a+10
+else:
+    a=a+20
+print(a)
+
+
+total_bill = 899
+if total_bill >1000:
+    print("20 percentage discount")
+elif total_bill>500:
+    print("10 percentage discount")
+else:
+    print("  no discount  ")    
+
+a1 = 10
+a2 = 20
+a3 = 30
+
+if (a1 >= 10):
+    print("yo")
+if (a2>=20):
+    print("yo")    
+if (a3>=30):
+    print("yo")    
+
+a1 = 10
+a2 = 20
+a3 = 30
+
+if (a1 >= 10):
+    print("yo")
+elif (a2>=20):
+    print("yo")    
+elif (a3>=30):
+    print("yo")    
+
+
+
+
