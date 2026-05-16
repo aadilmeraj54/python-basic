1fd4e5f6d2b0f1ad9ed35ac05ebc78b0ed15b31c
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-17 00:30:00 +0530

    Create set_list_tuple.py

---
+++ set_list_tuple.py
@@ -0,0 +1,22 @@
+s = {1,5,7,8,9,4,1,5,7}
+print (s)
+
+
+print(9 in s)
+print(1 in s)
+
+print(34 in s)
+
+# Functions in set
+
+d=[1,2,3,4,2,5,6,89]
+print(d)
+print(set(d))
+
+setA={1,2,3,4,5,6,89}
+setA.add(12)
+print(setA)
+
+t={"apple", "banana", "cherry"}
+for x in t:
+    print(x)
