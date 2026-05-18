d4cd11ebc9fa1abc26d32fac2f5b82da2a803c26
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-18 13:04:25 +0530

    Create loop_function.py

---
+++ loop_function.py
@@ -0,0 +1,109 @@
+# # (5>2)
+# # (5==2)
+# #(5!=2)
+# # (5<2)
+
+# ANY LOOP>>>START CONDITION 0, STOP CONDITION 10, STEPS +1
+
+
+# # SYNTAX
+# # WHILE LOOOOOOPPPPPPP
+
+# starting_point = 0
+# stoping_point = 10
+# while(starting_point < stoping_point):
+#     print("We are at stone no.", starting_point)
+#     starting_point=starting_point+1
+
+
+
+# #APPROACH  OF PRINTING EVEN NUMBERS
+# start = 0
+# stop = 100
+# count=0
+# while(start< stop):
+#     if(start%2==0):
+#         print(start)
+#     count= count+1    
+#     start=start+1    
+# print("My loop run" ,count ,"times")    
+
+
+# #APPROACH 2
+# start = 0
+# stop = 100
+# count=0
+# while(start< stop):
+#     print ("start")
+#     count= count+1    
+#     start=start+2  
+# print("My loop run" ,count ,"times")    
+
+
+# count=0
+# i=12
+# j=67
+# while 2*i<j :
+#     print(i)
+#     count=count+1
+#     i=i+1 
+# print(count)    
+
+
+
+
+
+
+##FOR LOOOOOOOPPPPPPPP
+
+# for i in range (0,10,1):
+#     print(i)
+
+
+# count=0
+# for i in range (17,47,3):
+#     print("yoyo", i)
+#     count=count+1
+# print(count)    
+
+
+
+
+# s=""
+# for i in range (1,6):
+#     s+=str(i)
+#     print(s)
+
+
+
+##FUNCTIONS
+
+# def eating_habbits():
+#     print("wash your hands daily")
+#     print("Twis in a day")
+#     print("After and before each meal")
+
+# eating_habbits()    
+# eating_habbits()    
+# eating_habbits()    
+# eating_habbits()    
+# eating_habbits()    
+# eating_habbits()    
+# eating_habbits()    
+
+
+def sum():
+    a=19
+    b=91
+    print(a+(2*b))
+
+for i in range(0,3):
+    sum()
+
+sum()    
+sum()    
+sum()    
+sum()    
+sum()    
+sum()    
+sum()    
