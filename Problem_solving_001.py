75cf6e5666df088ddeecc2a20dd1c6aaadbd4dd0
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-18 13:04:28 +0530

    Create Problem_solving_001.py

---
+++ Problem_solving_001.py
@@ -0,0 +1,15 @@
+##Q.1 You are making a billing system for a grocery store.
+# The store has a dictionary of items with their prices.
+# Write a function calculate_bill(cart, price_list) that:
+# • Takes a cart (list of items),
+# • Calculates and returns the total bill.
+
+
+price_list = {
+    "milk":50,
+    "bread":30,
+    "butter":90,
+    "eggs":60
+}
+
+cart=["milk","bread","eggs","milk"]
