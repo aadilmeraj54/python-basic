454638040d7457c8a8707143317a55d11a9df686
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-20 23:16:36 +0530

    Update while_loop.py

--- while_loop.py
+++ while_loop.py
@@ -1,4 +1,14 @@
-i =1 
-while(1):
-    print("welcome to conition less loop")
-    i +=1k
+# ###infinite loop
+# i = 1 
+# while(i<10):
+#     print("welcome to conition less loop")
+#     print(i)
+ 
+
+ ##### Nested Loop
+
+for hour_hand in range (1,25,1):
+    for min_hand  in range (1,61,1):
+        print (hour_hand, "hour", min_hand, "min")
+
+
