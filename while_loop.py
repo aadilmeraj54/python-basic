16a8489c5825c01a78e7acc515fd6bf430156a1a
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-21 20:16:00 +0530

    Update while_loop.py

--- while_loop.py
+++ while_loop.py
@@ -17,3 +17,6 @@ for i in range (0,60,2):
         if (i%j==0):
             print(i)
 
+for golgappa in range (1,11,1):
+    for person in range (1,5,1):
+        print("person",person, "had their golgappa number ", golgappa )
