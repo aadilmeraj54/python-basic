03df60089e5f62eada25d5af99047c444b40c6d7
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-22 23:08:53 +0530

    Update while_loop.py

--- while_loop.py
+++ while_loop.py
@@ -11,13 +11,13 @@
 #         print (hour_hand, "hour", min_hand, "min")
 
 
-#####
+# ####
 # for i in range (0,60,2):
 #     for j in range(2,3,1):
 #         if (i%j==0):
 #             print(i)
 
-####
+# ###
 # for golgappa in range (1,11,1):
 #     for person in range (1,5,1):
 #         print("person",person, "had their golgappa number ", golgappa )
