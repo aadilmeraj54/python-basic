35bf25f13a75e5152e425d191225b7e409916d97
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-25 23:43:03 +0530

    Update NoCodeDay.py

--- NoCodeDay.py
+++ NoCodeDay.py
@@ -189,7 +189,26 @@
 # sum = a + b
 # print(sum)
 
-fruits = ["apple", "banana", "mango"]
-for fruit in fruits:
-    print(fruit)
-print("Finished")
+# fruits = ["apple", "banana", "mango"]
+# for fruit in fruits:
+#     print(fruit)
+# print("Finished")
+
+
+import random
+
+secret_number = random.randint(1, 10)
+
+print("Welcome to the Number Guessing Game!")
+print("Guess a number between 1 and 10")
+
+while True:
+    guess = int(input("Enter your guess: "))
+
+    if guess == secret_number:
+        print("🎉 Correct! You guessed the number.")
+        break
+    elif guess < secret_number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
