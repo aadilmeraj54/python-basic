9a25c8ff552bf626fd245f155f1be9d8f7fd51c9
aadilmeraj54 <aadilmeraj54@gmail.com>
2026-05-29 23:04:25 +0530

    Update questionBilling.py

--- questionBilling.py
+++ questionBilling.py
@@ -1,28 +1,28 @@
-######Question: Supermarket Billing System
-## A supermarket keeps track of customer purchases each day.
-## Each customer can buy multiple different products, and each product has a quantity and price per unit.
-## Write a program that does the following:
-## • For each customer:
-## • Ask for the number of products they bought.
-## • For each product:
-## • Ask for the product name, quantity, and price per unit.
-## • Calculate the total price for that product (quantity x price).
-## • After all products are entered for a customer, display:
-## • The list of all products with their quantities and total prices.
-## • The total bill for that customer.
-## • Do this for multiple customers (e.g., until you say "no" to adding a new customer).
-## Extra:
-## Himanshu Sharma
-## At the end, display the grand total of all customers' bills combined.
+# ######Question: Supermarket Billing System
+# ## A supermarket keeps track of customer purchases each day.
+# ## Each customer can buy multiple different products, and each product has a quantity and price per unit.
+# ## Write a program that does the following:
+# ## • For each customer:
+# ## • Ask for the number of products they bought.
+# ## • For each product:
+# ## • Ask for the product name, quantity, and price per unit.
+# ## • Calculate the total price for that product (quantity x price).
+# ## • After all products are entered for a customer, display:
+# ## • The list of all products with their quantities and total prices.
+# ## • The total bill for that customer.
+# ## • Do this for multiple customers (e.g., until you say "no" to adding a new customer).
+# ## Extra:
+# ## Himanshu Sharma
+# ## At the end, display the grand total of all customers' bills combined.
 
 
 
-# name = input()
-# print("The name that you entered into the variable name is ", name)
-# print(type(name))
-# age=(input())
-# print("The age that you enterd into the age variable is", age)
-# print(type(age))
+# # name = input()
+# # print("The name that you entered into the variable name is ", name)
+# # print(type(name))
+# # age=(input())
+# # print("The age that you enterd into the age variable is", age)
+# # print(type(age))
 
 grand_toatl = 0
 
